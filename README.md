720ff91cedc8a5c4d66f984aaa8001451afe9019
dwaskevich <d.waskevich@comcast.net>
2024-06-21 21:30:23 -0500

    added console input for github user name and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # python_BeautifulSoup
 HTML parsing with BeautifulSoup 4
+
+Simple Python script to list github repositories (and their url's) in the console window.
+
+Console input requests a github user name. Response method in requests package returns web page html.
+
+BeautifulSoup html parser is used to locate the 'a' tags associated with repositories (data-tab-item attribute = repositories) and build a url link to the user's repositories page (href.
+
+Another html request is issued with the extracted repo url and BeautifulSoup is again used to locate 'a' tags with repository names (itemprop attribute = name codeRepository) and url's (href).
+
+Finally, repository names and clickable url's are output to the console.
--- beautifulSoup.py
+++ beautifulSoup.py
@@ -1,19 +1,50 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://github.com/dwaskevich?tab=repositories'
+baseURL = 'https://github.com/'  # Github domain
 
-# Send a GET request to URL
+# get user input
+url = input("Enter Github user name: ")
+
+url = baseURL + url  # generate full url
+print(url)
+
+# Send a GET request to user's github landing page
 response = requests.get(url)
 
-html_doc = response.text
-soup = BeautifulSoup(html_doc, 'html.parser')
+# check for response
+if response.status_code != requests.codes.ok:
+    print('No response from ' + url)
+    sys.exit(1)
 
-numRepos = 0
+html_doc = response.text  # extract html contents
+soup = BeautifulSoup(html_doc, 'html.parser')  # use BeautifulSoup to parse html
 
+# variable to hold repo url
+repoURL = ''
+
+# find repo page url
+for tag in soup.find_all('a', {'data-tab-item': 'repositories'}):
+    repoURL = tag.get('href')
+
+# construct repositories url
+url = baseURL.removesuffix('/') + repoURL
+
+print('Repositories url link: ', url)  # put link in console
+
+# Send a GET request to repositories url
+response = requests.get(url)
+
+html_doc = response.text  # extract html contents
+soup = BeautifulSoup(html_doc, 'html.parser')  # use BeautifulSoup to parse html
+
+numRepos = 0  # variable to count number of repositories
+
+# find all public repositories, print names and url's
 for tag in soup.find_all('a', {'itemprop': 'name codeRepository'}):
-    print(tag.string, tag.get('href'))
+    # print(tag.string, tag.get('href'))
+    print(tag.string, baseURL.removesuffix('/') + tag.get('href'))
     numRepos += 1
 
 print(f'Number of repositories found = {numRepos}')
-
